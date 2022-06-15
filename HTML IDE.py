eb6f9d83515b15a152b3a7ea087be95991322858
Shivvesh <71574601+Shivvesh@users.noreply.github.com>
2022-06-15 11:53:51 -0500

    Add files via upload

--- HTML IDE.py
+++ HTML IDE.py
@@ -1,5 +1,7 @@
 from tkinter import * 
 from PIL import ImageTk, Image
+from tkinter import filedialog
+import os
 
 root = Tk()
 root.minsize(650,650)
@@ -21,7 +23,24 @@ input_file_name.place(relx = 0.46, rely = 0.03, anchor = CENTER)
 my_text = Text(root, height = 35, width = 80)
 my_text.place(relx = 0.5, rely = 0.55, anchor = CENTER)
 
-open_button = Button(root, image = open_img, text = "Open File")
+name = ""
+def openFile():
+    global name
+    my_text.delete(1.0, END)
+    input_file_name.delete(0, END)
+    text_file = filedialog.askopenfilename(title=" Open Text File", filetypes=(("Text Files", "*.txt"),))
+    print(text_file)
+    name = os.path.basename(text_file)
+    formated_name = name.split('.')[0]
+    input_file_name.insert(END,formated_name)
+    root.title(formated_name)
+    text_file = open(name,'r')
+    paragraph=text_file.read()
+    my_text.insert(END,paragraph)
+    text_file.close()
+    
+open_button = Button(root, image = open_img, text = "Open File" ,command = openFile)
 open_button.place(relx = 0.05, rely = 0.03, anchor = CENTER)
+    
 
 root.mainloop()
